e94152055a479fc42c243b0d64eadfabff1082e2
mahmood <mahmoodsh9440@gmail.com>
2021-12-10 17:42:26 +0330

    pwa and vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
+import router from "./router.js";
 import "./registerServiceWorker";
-import router from "./router";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+
+    pwa: {
+        name: "First App",
+        themeColor: "#400082",
+        // iconPaths: {
+        //     favicon32: 'img/app_icons/icon-32x32.png',
+        //     favicon16: 'img/app_icons/icon-16x16.png',
+        // },
+        manifestOptions: {
+            background_color: "#eeeeee",
+        }
+    }
+};
